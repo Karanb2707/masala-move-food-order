b89463e8cb4ba1089e29f0b40dfd08d5dee32ce7
Karanb2707 <95834281+Karanb2707@users.noreply.github.com>
2025-08-20 19:02:47 +0530

    Added User Icon

--- masala-move-app/src/components/Header.jsx
+++ masala-move-app/src/components/Header.jsx
@@ -4,15 +4,16 @@ import { IoMdCart } from "react-icons/io";
 import { NavLink } from 'react-router-dom';
 import { IoIosHome } from "react-icons/io";
 import { TiShoppingCart } from "react-icons/ti";
-import { IoCallSharp } from "react-icons/io5";
+import { IoCallSharp, IoLockOpenOutline, IoStatsChartOutline } from "react-icons/io5";
 import { IoPhonePortraitOutline } from "react-icons/io5";
+import { FaUserCircle } from 'react-icons/fa';
 
 const Header = () => {
   return (
     <div className='flex justify-evenly items-center p-2 shadow-sm shadow-gray-500'>
       <img src={logo} alt="Logo Image" className='w-[80px] h-[50px] cursor-pointer' />
       <nav>
-        <ul className='flex justify-evenly items-center gap-10 font-semibold'>
+        <ul className='flex gap-8 font-semibold'>
 
           <li className='hover:scale-110 transition duration-300 cursor-pointer text-[16px]'>
             <NavLink to='/' className={({ isActive }) => isActive ? 'text-orange-600' : ''}>
@@ -52,7 +53,17 @@ const Header = () => {
           
         </ul>
       </nav>
-      <IoMdCart className='text-3xl text-orange-600 cursor-pointer' />
+
+      <div className='flex gap-10'>
+        <IoMdCart className='text-3xl text-orange-600 cursor-pointer' />
+
+        <div className='flex gap-3 items-center'>
+          <FaUserCircle className='text-3xl text-orange-600 cursor-pointer' />
+          <button className='px-2 py-1 bg-orange-600 text-white ring ring-orange-700 rounded-md text-sm font-semibold cursor-pointer'>
+            Login
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
