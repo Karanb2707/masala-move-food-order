c658882870f3dc9afb24c214b111a3f26e6151e4
Karanb2707 <95834281+Karanb2707@users.noreply.github.com>
2025-09-30 18:54:34 +0530

    cors issue on local

--- masala-move-app/vite.config.js
+++ masala-move-app/vite.config.js
@@ -5,4 +5,17 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  // Proxy configuration to handle CORS issues during local
+  server: {
+    proxy: {
+      '/api/swiggy': {
+        target: 'https://www.swiggy.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/swiggy/, ''),
+        headers: {
+          'User-Agent': 'Mozilla/5.0'
+        }
+      }
+    }
+  }
 })
